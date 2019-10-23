7598d97ec5223f60b381dff5bc0f61f111d6b161
demur <demur2000@mail.ru>
2019-10-22 22:51:16 -0400

    exercise: 1.2 Notification Channels

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -50,15 +50,35 @@ class EggTimerFragment : Fragment() {
         binding.eggTimerViewModel = viewModel
         binding.lifecycleOwner = this.viewLifecycleOwner
 
-        // TODO: Step 1.7 call create channel
+        // DONE: Step 1.7 call create channel
+        createChannel(
+            getString(R.string.egg_notification_channel_id),
+            getString(R.string.egg_notification_channel_name)
+        )
 
         return binding.root
     }
 
     private fun createChannel(channelId: String, channelName: String) {
-        // TODO: Step 1.6 START create a channel
-
-        // TODO: Step 1.6 END create a channel
+        // DONE: Step 1.6 START create a channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                // TODO: Step 2.4 change importance
+                NotificationManager.IMPORTANCE_LOW
+            )
+            // TODO: Step 2.6 disable badges for this channel
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description =
+                getString(R.string.breakfast_notification_channel_description)
+            val notificationManager =
+                requireActivity().getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        // DONE: Step 1.6 END create a channel
 
     }
 
@@ -66,4 +86,3 @@ class EggTimerFragment : Fragment() {
         fun newInstance() = EggTimerFragment()
     }
 }
-
