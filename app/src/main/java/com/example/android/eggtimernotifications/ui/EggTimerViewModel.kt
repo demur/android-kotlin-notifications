22a6587f2f12e72bb4a94e550150b29765d8190a
demur <demur2000@mail.ru>
2019-10-22 23:25:57 -0400

    exercise: 1.3 Add Notifications to your App

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
@@ -119,11 +119,13 @@ class EggTimerViewModel(private val app: Application) : AndroidViewModel(app) {
                 val triggerTime = SystemClock.elapsedRealtime() + selectedInterval
 
                 // DONE: Step 1.5 get an instance of NotificationManager and call sendNotification
+                /*
                 val notificationManager = ContextCompat.getSystemService(
                     app,
                     NotificationManager::class.java
                 ) as NotificationManager
                 notificationManager.sendNotification(app.getString(R.string.timer_running), app)
+                */
 
                 // TODO: Step 1.15 call cancel notification
 
