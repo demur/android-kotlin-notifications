93b799e5a151e75849dd01c63f18c14c4436900b
demur <demur2000@mail.ru>
2019-10-23 23:09:53 -0400

    exercise: 2.2 Add Snooze Action

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -55,7 +55,10 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .bigPicture(eggImage)
         .bigLargeIcon(null)
 
-    // TODO: Step 2.2 add snooze action
+    // DONE: Step 2.2 add snooze action
+    val snoozeIntent = Intent(applicationContext, SnoozeReceiver::class.java)
+    val snoozePendingIntent =
+        PendingIntent.getBroadcast(applicationContext, REQUEST_CODE, snoozeIntent, FLAGS)
 
     // DONE: Step 1.2 get an instance of NotificationCompat.Builder
     // Build the notification
@@ -78,8 +81,12 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         // DONE: Step 2.1 add style to builder
         .setStyle(bigPicStyle)
         .setLargeIcon(eggImage)
-        // TODO: Step 2.3 add snooze action
-
+        // DONE: Step 2.3 add snooze action
+        .addAction(
+            R.drawable.egg_icon,
+            applicationContext.getString(R.string.snooze),
+            snoozePendingIntent
+        )
         // TODO: Step 2.5 set priority
 
     // DONE: Step 1.4 call notify
